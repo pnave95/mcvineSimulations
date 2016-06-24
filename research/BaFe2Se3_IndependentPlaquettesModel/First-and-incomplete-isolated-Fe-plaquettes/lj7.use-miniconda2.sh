353721206be4064f9948001e63278c34df19a6d4
Nave P O <p63@analysis-node09.sns.gov>
2016-06-23 22:45:18 -0400

    initial commit of mcvine simulation work and research at ORNL

---
+++ research/BaFe2Se3_IndependentPlaquettesModel/First-and-incomplete-isolated-Fe-plaquettes/lj7.use-miniconda2.sh
@@ -0,0 +1,3 @@
+unset PYTHONPATH
+# added by Miniconda2 4.0.5 installeru
+export PATH="/SNS/users/lj7/software/miniconda2/bin:$PATH"
---
+++ research/BaFe2Se3_IndependentPlaquettesModel/beam_20meV/run-m2s.sh
@@ -0,0 +1,1 @@
+mcvine instruments arcs mod2sample --ncount=1000000.0 --buffer_size=100000 --output-dir=_m2sout --moderator.S_filename=/SNS/users/lj7/software/miniconda2/envs/mcvine/share/mcvine/resources/instruments/ARCS/moderator/source_sct521_bu_17_1.dat
